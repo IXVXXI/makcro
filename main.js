0207294fa380fcbbeb0143a96449beb1e054b5c6
Will <ixvxxi@gmail.com>
2020-04-01 12:34:51 -0500

    wdawd

--- main.js
+++ main.js
@@ -66,7 +66,7 @@ let initClient = () => {
             discoveryDocs: [
                 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'
             ],
-            scope: 'https://www.googleapis.com/auth/drive.metadata.readonly'
+            scope: 'https://www.googleapis.com/auth/drive'
         })
         .then(() => {
             // ensureId()
